3b038cb11ff380c7eb3e9d352f773338fdd037fc
Kwett <coudrin.clement@gmail.com>
2024-06-06 14:04:09 +0200

    Bonus 1

--- src/Service/Checkout.php
+++ src/Service/Checkout.php
@@ -13,4 +13,25 @@ class Checkout
      * and the total price minus the discount applied (50-cent) for every three cupcakes purchased at the second index
      * ⬇️ create the calculate() method here ⬇️
      */
+    public const DISCOUNT = 0.5;
+    public const DISCOUNT_NUMBER = 3;
+    public function calculate(array $cart): array
+    {
+        $totalPrice = 0.0;
+        $discountedPrice = 0.0;
+        $totalItems = 0;
+
+        foreach ($cart as $item) {
+            $numberCupcakes = $item['numberCupcake'];
+            $unitPrice = $item['unitPrice'];
+            $itemTotal = $numberCupcakes * $unitPrice;
+            $totalPrice += $itemTotal;
+            $totalItems += $item['numberCupcake'];
+        }
+        $discountNumber = floor($totalItems / self::DISCOUNT_NUMBER);
+        $discountedPrice = $totalPrice - ($discountNumber * self::DISCOUNT);
+
+
+        return [$totalPrice, $discountedPrice];
+    }
 }
