0804cfc176ee13611e678cc29fe430a34418e075
Kwett <coudrin.clement@gmail.com>
2024-06-06 10:46:32 +0200

    Implements cupcake creation

--- src/Controller/CupcakeController.php
+++ src/Controller/CupcakeController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Service\Container;
+use App\Model\AccessoryManager;
+use App\Model\CupcakeManager;
 
 /**
  * Class CupcakeController
@@ -18,14 +20,29 @@ class CupcakeController extends AbstractController
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
+    private $accessoryManager;
+    private $cupcakeManager;
+    public function __construct()
+    {
+        parent::__construct();
+        $this->accessoryManager = new AccessoryManager();
+        $this->cupcakeManager = new CupcakeManager();
+    }
+
     public function add()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            //TODO Add your code here to create a new cupcake
+            $cupcake = array_map('htmlentities', array_map('trim', $_POST));
+            $this->cupcakeManager->insert($cupcake);
             header('Location:/cupcake/list');
         }
-        //TODO retrieve all accessories for the select options
-        return $this->twig->render('Cupcake/add.html.twig');
+        $accessories = $this->accessoryManager->selectAll();
+        return $this->twig->render(
+            'Cupcake/add.html.twig',
+            [
+                'accessories' => $accessories
+            ]
+        );
     }
 
     /**
---
+++ src/Model/CupcakeManager.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Model;
+
+use PDO;
+use DateTime;
+
+class CupcakeManager extends AbstractManager
+{
+    public const TABLE = "cupcake";
+
+    public function insert(array $cupcake): bool
+    {
+
+        $dateTime = new DateTime();
+        $createdAt = $dateTime->format('Y-m-d H:i:s');
+
+        $statement = $this->pdo->prepare(
+            "INSERT INTO " . self::TABLE .
+            " (`name`,`color1`,`color2`,`color3`,`accessory_id`,`created_at`)
+            VALUES (:name, :color1, :color2, :color3, :accessory_id, :created_at);"
+        );
+        $statement->bindValue("name", $cupcake["name"], PDO::PARAM_STR);
+        $statement->bindValue("color1", $cupcake["color1"], PDO::PARAM_STR);
+        $statement->bindValue("color2", $cupcake["color2"], PDO::PARAM_STR);
+        $statement->bindValue("color3", $cupcake["color3"], PDO::PARAM_STR);
+        $statement->bindValue("accessory_id", $cupcake["accessory"], PDO::PARAM_STR);
+        $statement->bindValue("created_at", $createdAt, PDO::PARAM_STR);
+
+        return $statement->execute();
+    }
+}
