3aec137e7b39bac93f5e2bfe9cc4ce46e3f29e3c
AntoineSPR <antoine.simper@gmail.com>
2024-06-06 11:23:25 +0200

    Added Checkout discount

--- src/Service/Checkout.php
+++ src/Service/Checkout.php
@@ -13,4 +13,18 @@ class Checkout
      * and the total price minus the discount applied (50-cent) for every three cupcakes purchased at the second index
      * ⬇️ create the calculate() method here ⬇️
      */
+
+    public function calculate(array $cart): array
+    {
+        $totalPrice = 0;
+        $totalCupcakes = 0;
+        foreach ($cart as $order) {
+            $totalPrice += $order['numberCupcake'] * $order['unitPrice'];
+            $totalCupcakes += $order['numberCupcake'];
+        }
+        $discountCounter = floor($totalCupcakes / 3);
+        $finalPrice = $totalPrice - ($discountCounter * 0.5);
+
+        return [$totalPrice, $finalPrice];
+    }
 }
