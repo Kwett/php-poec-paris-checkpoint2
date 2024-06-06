17311903fb65e65188b0ae1117e2ac34b945ede7
AntoineSPR <antoine.simper@gmail.com>
2024-06-06 11:54:46 +0200

    Displayed packages in logistics

--- src/Service/Container.php
+++ src/Service/Container.php
@@ -4,4 +4,18 @@ namespace App\Service;
 
 class Container
 {
+    public const SMALL = 2;
+    public const MEDIUM = 5;
+    public const LARGE = 8;
+
+    public function inbox(int $numberCake): array
+    {
+        $numberL = 0;
+        $numberM = 0;
+        $numberS = 0;
+
+        $numberL = floor($numberCake / self::LARGE);
+
+        return [$numberL, $numberM, $numberS];
+    }
 }
